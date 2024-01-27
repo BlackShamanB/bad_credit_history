87684939ea2034e94bdbcd17e2e535201446ab22
n777sam <n777sam@gmail.com>
2024-01-27 20:22:56 +0400

    amo form

--- js/form.js
+++ js/form.js
@@ -1,7 +1,9 @@
-import { ref, h } from 'vue';
+import { ref, h } from "vue";
 
 function loadScript(options, root = document.head) {
-  return root.appendChild(Object.assign(document.createElement('script'), options));
+  return root.appendChild(
+    Object.assign(document.createElement("script"), options)
+  );
 }
 
 export const Comp = defineComponent(() => {
@@ -10,24 +12,20 @@ export const Comp = defineComponent(() => {
   onMounted(() => {
     loadScript(
       {
-        innerHTML: `
-        !function(a,m,o,c,r,m){a[o+c]=a[o+c]||
-            {setMeta:function(p){this.params=(this.params||[]).concat([p])}},a[o+r]=a[o+r]||function(f){a[o+r].f=(a[o+r].f||[]).concat([f])},a[o+r]({id:"1259802",hash:"9935402923c8f47b86bd9a223578ef04",locale:"ru"}
-            ),a[o+m]=a[o+m]||function(f,k){a[o+m].f=(a[o+m].f||[]).concat([[f,k]])}}(window,0,"amo_forms_","params","load","loaded");
-      `
+        innerHTML: `!function(a,m,o,c,r,m){a[o+c]=a[o+c]||{setMeta:function(p){this.params=(this.params||[]).concat([p])}},a[o+r]=a[o+r]||function(f){a[o+r].f=(a[o+r].f||[]).concat([f])},a[o+r]({id:"1278190",hash:"c45ebf4b10669d26e216a8ca11336d6f",locale:"ru"}),a[o+m]=a[o+m]||function(f,k){a[o+m].f=(a[o+m].f||[]).concat([[f,k]])}}(window,0,"amo_forms_","params","load","loaded");`,
       },
       root.value
     );
     loadScript(
       {
-        id: 'amoforms_script_1259802',
-        async: 'async',
-        charset: 'utf-8',
-        src: 'https://forms.amocrm.ru/forms/assets/js/amoforms.js?1703018780'
+        id: "amoforms_script_1278182",
+        async: "async",
+        charset: "utf-8",
+        src: "https://forms.amocrm.ru/forms/assets/js/amoforms.js?1706371047",
       },
       root.value
     );
   });
 
-  return () => h('div', { ref: root });
+  return () => h("div", { ref: root });
 });
